7a137c34340145b11f915febe9e265785e08afb1
Jennifer Silva <jennifersilva20@gmail.com>
2019-09-04 16:37:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,12 @@
-def my_each(collection)  # put argument(s) here
+def my_each(array)
+  i = 0 
+  while i < array.length
+  i = i + 1 
+end 
+
+def my_each(array)  # put argument(s) here
   do |i|
     puts i 
 end
+
+
